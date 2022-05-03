d3f63965e179c4ed51a7a24938d51c214f660e23
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-03 16:45:21 +0100

    Handle `Widget` button accessibility

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,18 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { useCallback, useState } from "react";
+
+import { Popover } from "@headlessui/react";
 
 export function Widget() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleVisibility = useCallback(() => setIsOpen((state) => !state), []);
-
   return (
-    <div className="absolute bottom-5 right-5">
-      {isOpen && <p>Hello Wolf</p>}
-
-      <button
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-        onClick={toggleVisibility}
-      >
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hello Wolf</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2" />
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
