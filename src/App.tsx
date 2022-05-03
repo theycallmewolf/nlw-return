4c4fca758d1ebc15e650ebbbb584b42fd7a1114d
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-03 15:56:45 +0100

    `Tailwind` setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello Wolf!</h1>;
+  return (
+    <h1 className="bg-slate-100 px-3 py-8 flex text-6xl text-cyan-700 hover:bg-slate-900 transition-colors">
+      Hello Wolf!
+    </h1>
+  );
 }
 
 export default App;
