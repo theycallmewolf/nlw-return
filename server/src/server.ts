e532eaa75a167e884978e93aeef3e726fd315478
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-04 18:01:29 +0100

    Email send setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6ee10e5b19138c",
+    pass: "2d3e5aa88bb7b0",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,13 +25,26 @@ app.post("/feedbacks", async (req, res) => {
     },
   });
 
+  await transport.sendMail({
+    from: process.env.EMAIL_FROM,
+    to: process.env.EMAIL_TO,
+    subject: "novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      "<h2>Novo Feedback</h2>",
+      `<p>Tipo: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
   return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
   console.log(`
-  -----
-  server running on port 3333
-  -----
-`);
+      -----
+      server running on port 3333
+      -----
+    `);
 });
