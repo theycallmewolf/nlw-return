faf63182ad39f25f3fe8932f84973c788cebd12c
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-04 11:46:31 +0100

    Adds `ScreenshotButton`

---
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -0,0 +1,59 @@
+import { Camera, Trash } from "phosphor-react";
+import { useCallback, useState } from "react";
+import html2canvas from "html2canvas";
+import { Loading } from "../Loading";
+import { backgroundSize } from "html2canvas/dist/types/css/property-descriptors/background-size";
+
+interface ScreenshotButtonProps {
+  screenshot: string | null;
+  setScreenshot: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export function ScreenshotButton({
+  setScreenshot,
+  screenshot,
+}: ScreenshotButtonProps) {
+  const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
+
+  const takeScreenshot = useCallback(async () => {
+    setIsTakingScreenshot(true);
+    const canvas = await html2canvas(document.querySelector("html")!);
+    const base64image = canvas.toDataURL("image/png");
+
+    setScreenshot(base64image);
+    setIsTakingScreenshot(false);
+  }, []);
+
+  const removeScreenshot = useCallback(() => setScreenshot(null), []);
+
+  if (screenshot)
+    return (
+      <button
+        type="button"
+        onClick={removeScreenshot}
+        className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        style={{
+          backgroundImage: `url(${screenshot})`,
+          // DEV
+          backgroundPosition: "right bottom",
+          backgroundSize: 180,
+        }}
+      >
+        <Trash weight="fill" />
+      </button>
+    );
+
+  return (
+    <button
+      type="button"
+      onClick={takeScreenshot}
+      className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+    >
+      {isTakingScreenshot ? (
+        <Loading />
+      ) : (
+        <Camera className="w-6 h-6 text-zinc-100" />
+      )}
+    </button>
+  );
+}
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft, Camera } from "phosphor-react";
+import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -11,6 +13,8 @@ export function FeedbackContentStep({
   feedbackType,
   handleFeedbackReset,
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -39,12 +43,10 @@ export function FeedbackContentStep({
             placeholder="Descreva a sua dúvida"
           />
           <footer className="flex gap-2 mt-2">
-            <button
-              type="button"
-              className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
-            >
-              <Camera className="w-6 h-6 text-zinc-100" />
-            </button>
+            <ScreenshotButton
+              setScreenshot={setScreenshot}
+              screenshot={screenshot}
+            />
             <button
               type="submit"
               className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
