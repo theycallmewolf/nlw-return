80958ad34a5cada16cd9af582422f3634322e719
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-05 11:16:16 +0100

    Adds `SubmitFeedbackUseCase` remaining tests

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,25 +1,55 @@
-import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+    import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
-const submitFeedback = new SubmitFeedbackUseCase(
-  {
-    create: async () => ({
-      id: "1",
-      type: "BUG",
-      comment: "mock",
-      screenshot: "mock.jpeg",
-    }),
-  },
-  { sendMail: async () => {} }
-);
+    const submitFeedback = new SubmitFeedbackUseCase(
+      {
+        create: async () => ({
+          id: "1",
+          type: "BUG",
+          comment: "mock",
+          screenshot: "mock.jpeg",
+        }),
+      },
+      { sendMail: async () => {} }
+    );
 
-describe("submit feedback", () => {
-  it("should be able to submit a feedback", async () => {
-    await expect(
-      submitFeedback.execute({
-        type: "BUG",
-        comment: "mock",
-        screenshot: "data:image/png;base64,MOCK",
-      })
-    ).resolves.not.toThrow();
-  });
-});
+    describe("submit feedback", () => {
+      it("should be able to submit a feedback", async () => {
+        await expect(
+          submitFeedback.execute({
+            type: "BUG",
+            comment: "mock",
+            screenshot: "data:image/png;base64,MOCK",
+          })
+        ).resolves.not.toThrow();
+      });
+
+      it("should't be able to submit a feedback without a `type`", async () => {
+        await expect(
+          submitFeedback.execute({
+            type: "",
+            comment: "mock",
+            screenshot: "data:image/png;base64,MOCK",
+          })
+        ).rejects.toThrow();
+      });
+
+      it("should't be able to submit a feedback without a `comment`", async () => {
+        await expect(
+          submitFeedback.execute({
+            type: "BUG",
+            comment: "",
+            screenshot: "data:image/png;base64,MOCK",
+          })
+        ).rejects.toThrow();
+      });
+
+      it("should't be able to submit a feedback with an invalid `screenshot`", async () => {
+        await expect(
+          submitFeedback.execute({
+            type: "BUG",
+            comment: "mock",
+            screenshot: "INVALID",
+          })
+        ).rejects.toThrow();
+      });
+    });
