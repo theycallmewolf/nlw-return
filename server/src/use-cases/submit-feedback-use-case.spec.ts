94fce8e79cf842df802ba1616f336855edfaeb7b
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-05 11:02:42 +0100

    Refactor `SubmitFeedbackUseCase` test

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,19 +1,19 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
+const submitFeedback = new SubmitFeedbackUseCase(
+  {
+    create: async () => ({
+      id: "1",
+      type: "BUG",
+      comment: "mock",
+      screenshot: "mock.jpeg",
+    }),
+  },
+  { sendMail: async () => {} }
+);
+
 describe("submit feedback", () => {
   it("should be able to submit a feedback", async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      {
-        create: async () => ({
-          id: "1",
-          type: "BUG",
-          comment: "mock",
-          screenshot: "mock.jpeg",
-        }),
-      },
-      { sendMail: async () => {} }
-    );
-
     await expect(
       submitFeedback.execute({
         type: "BUG",
