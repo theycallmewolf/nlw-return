3a93e0bd1614edfc83cc72983d3532282f0b1e38
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-05 16:43:27 +0100

    Adds `ScreenshotButton`

---
+++ mobile/src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,37 @@
+import { Camera, Trash } from "phosphor-react-native";
+import React from "react";
+
+import { View, TouchableOpacity, Image } from "react-native";
+import { theme } from "../../theme";
+
+import { styles } from "./styles";
+
+interface Props {
+  screenshot: string | null;
+  onTakeShot: () => void;
+  onRemoveShot: () => void;
+}
+
+export function ScreenshotButton({
+  screenshot,
+  onRemoveShot,
+  onTakeShot,
+}: Props) {
+  return (
+    <TouchableOpacity
+      onPress={screenshot ? onRemoveShot : onTakeShot}
+      style={styles.container}
+    >
+      {screenshot ? (
+        <Trash
+          size={22}
+          color={theme.colors.text_secondary}
+          weight="fill"
+          style={styles.removeIcon}
+        />
+      ) : (
+        <Camera size={22} color={theme.colors.text_secondary} weight="bold" />
+      )}
+    </TouchableOpacity>
+  );
+}
