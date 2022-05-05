c7c0300efb18c0cf1ef8c8a1db91b8416949b24f
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-05 15:46:48 +0100

    Updates `Widget` position

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -30,8 +30,8 @@ export default function App() {
         backgroundColor: theme.colors.background,
       }}
     >
-      <Widget />
       <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget />
     </View>
   );
 }
