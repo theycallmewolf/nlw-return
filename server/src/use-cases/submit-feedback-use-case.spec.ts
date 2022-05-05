fd47dd70c57b7da6d2623eded4f7664cc5d7b485
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-05 11:27:14 +0100

    Adds `SubmitFeedbackUseCase`  test spyes

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,55 +1,54 @@
-    import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
-    const submitFeedback = new SubmitFeedbackUseCase(
-      {
-        create: async () => ({
-          id: "1",
-          type: "BUG",
-          comment: "mock",
-          screenshot: "mock.jpeg",
-        }),
-      },
-      { sendMail: async () => {} }
-    );
+const createFeedbackSpy = jest.fn();
+const sendEmailSpy = jest.fn();
 
-    describe("submit feedback", () => {
-      it("should be able to submit a feedback", async () => {
-        await expect(
-          submitFeedback.execute({
-            type: "BUG",
-            comment: "mock",
-            screenshot: "data:image/png;base64,MOCK",
-          })
-        ).resolves.not.toThrow();
-      });
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackSpy },
+  { sendMail: sendEmailSpy }
+);
 
-      it("should't be able to submit a feedback without a `type`", async () => {
-        await expect(
-          submitFeedback.execute({
-            type: "",
-            comment: "mock",
-            screenshot: "data:image/png;base64,MOCK",
-          })
-        ).rejects.toThrow();
-      });
+describe("submit feedback", () => {
+  it("should be able to submit a feedback", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "mock",
+        screenshot: "data:image/png;base64,MOCK",
+      })
+    ).resolves.not.toThrow();
 
-      it("should't be able to submit a feedback without a `comment`", async () => {
-        await expect(
-          submitFeedback.execute({
-            type: "BUG",
-            comment: "",
-            screenshot: "data:image/png;base64,MOCK",
-          })
-        ).rejects.toThrow();
-      });
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendEmailSpy).toHaveBeenCalled();
+  });
 
-      it("should't be able to submit a feedback with an invalid `screenshot`", async () => {
-        await expect(
-          submitFeedback.execute({
-            type: "BUG",
-            comment: "mock",
-            screenshot: "INVALID",
-          })
-        ).rejects.toThrow();
-      });
-    });
+  it("should't be able to submit a feedback without a `type`", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "",
+        comment: "mock",
+        screenshot: "data:image/png;base64,MOCK",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should't be able to submit a feedback without a `comment`", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "",
+        screenshot: "data:image/png;base64,MOCK",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should't be able to submit a feedback with an invalid `screenshot`", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "mock",
+        screenshot: "INVALID",
+      })
+    ).rejects.toThrow();
+  });
+});
