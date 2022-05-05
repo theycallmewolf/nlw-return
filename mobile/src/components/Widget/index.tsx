a398b7f76361cc95b4d6344407c89c902703d3ad
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-05 15:56:08 +0100

    Adds `BottomSheet` base to `Widget`

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import { TouchableOpacity } from "react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 import { ChatTeardropDots } from "phosphor-react-native";
 
@@ -7,11 +9,27 @@ import { styles } from "./styles";
 import { theme } from "../../theme";
 
 export function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleOpen = () => {
+    bottomSheetRef.current?.expand();
+  };
+
   return (
     <>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={handleOpen} style={styles.button}>
         <ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} />
       </TouchableOpacity>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
