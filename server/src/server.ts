db9ec1d7bf52bae0ab26f2145c50a3f1fe8abfc7
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-08 22:02:32 +0100

    Server `PORT` env setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,10 +12,10 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log(`
       -----
-      server running on port 3333
+      server running on port ${process.env.PORT || 3333}
       -----
     `);
 });
