191524848108699855bd1f1af89f6675de02af1c
theycallmewolf <bruno.m.lobato@gmail.com>
2022-05-08 11:44:51 +0100

    Setup `body parser`

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import { routes } from "./routes";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(bodyParser.urlencoded({ limit: "2mb", extended: true }));
+app.use(bodyParser.text({ limit: "2mb" }));
 app.use(express.json());
 
 app.use(routes);
